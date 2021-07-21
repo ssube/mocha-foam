a0ce6c579dfafc950d7c1acca8778ccfa4bf02ad
ssube <seansube@gmail.com>
2021-07-20 23:50:21 -0500

    add an example uuid test

--- test/TestOver.ts
+++ test/TestOver.ts
@@ -1,4 +1,4 @@
-import { integer } from 'fast-check';
+import { integer, uuid } from 'fast-check';
 
 import { over } from '../src/index';
 
@@ -6,6 +6,12 @@ describe('some foo', () => {
   over('the bars', integer(), (it) => {
     it('should be a small number', (bar: number) => {
       return bar < 1_000;
+    });
+  });
+
+  over('some IDs', uuid(), (it) => {
+    it('should be a good one', (id: string) => {
+      return id[9] !== 'a';
     });
   });
 });
