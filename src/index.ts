71c0064703e5c87d9611647442cc70ea402ec079
ssube <seansube@gmail.com>
2021-07-25 12:34:47 -0500

    mark string guard as private

--- src/index.ts
+++ src/index.ts
@@ -122,6 +122,8 @@ export function isErrorRun<T>(details: RunDetails<T>): boolean {
 
 /**
  * Typeguard for string primitives.
+ *
+ * @private
  */
 export function isString(val: unknown): val is string {
   return typeof val === 'string';
